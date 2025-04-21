858927c75c2e502efde5b4f28fb3f5f859c0491a
Dipayan Bose <bosedipayan2000@gmail.com>
2025-04-21 12:41:52 +0530

    First commit for IRCTC project

---
+++ app/src/main/java/ticket/booking/App.java
@@ -0,0 +1,11 @@
+package ticket.booking;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new org.example.App().getGreeting());
+    }
+}
