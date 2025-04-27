ed50d6fed93652573625df24a2fd985a2389793f
Dipayan Bose <bosedipayan2000@gmail.com>
2025-04-27 13:27:09 +0530

    Just learning and modification with streams

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -1,11 +1,20 @@
 package ticket.booking;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new org.example.App().getGreeting());
+
+//        System.out.println(new org.example.App().getGreeting());
+
+        List<Integer> arr = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        List<Integer> l = arr.stream().filter(e -> e%2 == 0).collect(Collectors.toList());
     }
 }
