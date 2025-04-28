b0d3c5e02cfdda57f5b128f9b97ec0d259258f10
Dipayan Bose <bosedipayan2000@gmail.com>
2025-04-28 09:01:14 +0530

    Setting up of getter and setter for the entities because we use private access specifier

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -11,10 +11,5 @@ public class App {
 
     public static void main(String[] args) {
 
-//        System.out.println(new org.example.App().getGreeting());
-
-        List<Integer> arr = Arrays.asList(1,2,3,4,5,6,7,8,9);
-
-        List<Integer> l = arr.stream().filter(e -> e%2 == 0).collect(Collectors.toList());
     }
 }
